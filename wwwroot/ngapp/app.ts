a00066c5bd5c017186247abec72072245d43ba0d
Rick Cohen <rickco@gmail.com>
2017-04-04 18:30:44 -0400

    angular project

---
+++ wwwroot/ngapp/app.ts
@@ -0,0 +1,19 @@
+﻿namespace MyApp {
+
+    angular.module('MyApp', ['ui.router']).config(($stateProvider: ng.ui.IStateProvider, $locationProvider: ng.ILocationProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) => {
+        $stateProvider.state('Home', {
+            url: '/',
+            templateUrl: '/ngApp/products.html',
+            controller: MyApp.Controllers.ProductListController,
+            controllerAs: 'controller'
+        }).state('Details', {
+            url: '/details/:id',
+            templateUrl: '/ngApp/details.html',
+            controller: MyApp.Controllers.ProductDetailsController,
+            controllerAs: 'controller'
+        });
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(true);
+    });
+
+}
